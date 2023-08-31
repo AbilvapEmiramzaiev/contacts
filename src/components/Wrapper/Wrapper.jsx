e1e0bfbea3be8dd362e4c1038555f9ebb4bc394b
AbilvapEmiramzaiev <emiramzaev.ab@yahoo.com>
2023-08-31 19:46:35 +0200

    create contact form

--- src/components/Wrapper/Wrapper.jsx
+++ src/components/Wrapper/Wrapper.jsx
@@ -1,14 +1,25 @@
 import './Wrapper.css';
 import ContactItem from '../ContactItem/ContactItem';
-import {contacts} from '.../data/static.contacts.db.js'
+import { contacts as contactsData } from '../../data/static.contacts.db.js'
+import CreateContactForm from '../CreateContactForm/CreateContactForm';
+import { useState } from 'react';
 function Wrapper() {
+
+    const [contacts, setContacts] = useState(contactsData)
 
     return (
         <div className="wrapper">
             <h2>Contact list</h2>
-            <div>{contacts.map((el, index) => (
-                <ContactItem key={index} name={el.name} email={el.email} />
-            ))}
+            <CreateContactForm setContacts={setContacts}/>
+            <div>
+                {contacts.length ? 
+                    (contacts.map(el => (
+                        <ContactItem key={el.id} name={el.name} email={el.email} />
+                    ))) :
+                    (
+                        <p>There are no contacts.</p>
+                    )
+                }
             </div>
         </div>
     )
