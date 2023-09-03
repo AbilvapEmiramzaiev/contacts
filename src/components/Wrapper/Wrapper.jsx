2f8e98c24c7e943cc2b6fae3c33dfeb2a12bd976
AbilvapEmiramzaiev <emiramzaev.ab@yahoo.com>
2023-09-03 15:56:41 +0200

    search-bar

--- src/components/Wrapper/Wrapper.jsx
+++ src/components/Wrapper/Wrapper.jsx
@@ -5,21 +5,35 @@ import CreateContactForm from '../CreateContactForm/CreateContactForm';
 import { useEffect, useState } from 'react';
 import { ContactService } from '../../services/contacts.service';
 import { Link } from 'react-router-dom';
+import SearchBar from '../SearchBar/SearchBar';
 function Wrapper() {
+    
     const [contacts, setContacts] = useState([])
+    const [filteredContacts, setFilteredContacts] = useState([])
+
     useEffect(()=>{
         const fetchContacts = async ()=>{
             const response = await ContactService.getAll();
             setContacts(response);
+            setFilteredContacts(response);
         }
         fetchContacts();
     }, [])
+
+    const handleSearch = (pattern) => {
+        const filtered = contacts.filter((contact)=>
+            contact.name.toLowerCase().includes(pattern.toLowerCase())    
+        )
+        setFilteredContacts(filtered);
+    }
+
     return (
         <div className="wrapper">
             <h2>Contact list</h2>
+            <SearchBar onSearch={ handleSearch }/>
             <div>
-                {contacts.length ? 
-                    (contacts.map(el => (
+                {filteredContacts.length ? 
+                    (filteredContacts.map(el => (
                         <ContactItem key={el.id} name={el.name} email={el.email} />
                     ))) :
                     (
